9bf0f2b8ca234495b394ec677a9d48a8e90fb719
Hanzo Asashi <hansenmakangiras@gmail.com>
2022-07-04 02:50:37 +0800

    update and add many files

---
+++ storage/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,103 @@
+<?php return array (
+  'sans-serif' => array(
+    'normal' => $rootDir . '/lib/fonts/Helvetica',
+    'bold' => $rootDir . '/lib/fonts/Helvetica-Bold',
+    'italic' => $rootDir . '/lib/fonts/Helvetica-Oblique',
+    'bold_italic' => $rootDir . '/lib/fonts/Helvetica-BoldOblique',
+  ),
+  'times' => array(
+    'normal' => $rootDir . '/lib/fonts/Times-Roman',
+    'bold' => $rootDir . '/lib/fonts/Times-Bold',
+    'italic' => $rootDir . '/lib/fonts/Times-Italic',
+    'bold_italic' => $rootDir . '/lib/fonts/Times-BoldItalic',
+  ),
+  'times-roman' => array(
+    'normal' => $rootDir . '/lib/fonts/Times-Roman',
+    'bold' => $rootDir . '/lib/fonts/Times-Bold',
+    'italic' => $rootDir . '/lib/fonts/Times-Italic',
+    'bold_italic' => $rootDir . '/lib/fonts/Times-BoldItalic',
+  ),
+  'courier' => array(
+    'normal' => $rootDir . '/lib/fonts/Courier',
+    'bold' => $rootDir . '/lib/fonts/Courier-Bold',
+    'italic' => $rootDir . '/lib/fonts/Courier-Oblique',
+    'bold_italic' => $rootDir . '/lib/fonts/Courier-BoldOblique',
+  ),
+  'helvetica' => array(
+    'normal' => $rootDir . '/lib/fonts/Helvetica',
+    'bold' => $rootDir . '/lib/fonts/Helvetica-Bold',
+    'italic' => $rootDir . '/lib/fonts/Helvetica-Oblique',
+    'bold_italic' => $rootDir . '/lib/fonts/Helvetica-BoldOblique',
+  ),
+  'zapfdingbats' => array(
+    'normal' => $rootDir . '/lib/fonts/ZapfDingbats',
+    'bold' => $rootDir . '/lib/fonts/ZapfDingbats',
+    'italic' => $rootDir . '/lib/fonts/ZapfDingbats',
+    'bold_italic' => $rootDir . '/lib/fonts/ZapfDingbats',
+  ),
+  'symbol' => array(
+    'normal' => $rootDir . '/lib/fonts/Symbol',
+    'bold' => $rootDir . '/lib/fonts/Symbol',
+    'italic' => $rootDir . '/lib/fonts/Symbol',
+    'bold_italic' => $rootDir . '/lib/fonts/Symbol',
+  ),
+  'serif' => array(
+    'normal' => $rootDir . '/lib/fonts/Times-Roman',
+    'bold' => $rootDir . '/lib/fonts/Times-Bold',
+    'italic' => $rootDir . '/lib/fonts/Times-Italic',
+    'bold_italic' => $rootDir . '/lib/fonts/Times-BoldItalic',
+  ),
+  'monospace' => array(
+    'normal' => $rootDir . '/lib/fonts/Courier',
+    'bold' => $rootDir . '/lib/fonts/Courier-Bold',
+    'italic' => $rootDir . '/lib/fonts/Courier-Oblique',
+    'bold_italic' => $rootDir . '/lib/fonts/Courier-BoldOblique',
+  ),
+  'fixed' => array(
+    'normal' => $rootDir . '/lib/fonts/Courier',
+    'bold' => $rootDir . '/lib/fonts/Courier-Bold',
+    'italic' => $rootDir . '/lib/fonts/Courier-Oblique',
+    'bold_italic' => $rootDir . '/lib/fonts/Courier-BoldOblique',
+  ),
+  'dejavu sans' => array(
+    'bold' => $rootDir . '/lib/fonts/DejaVuSans-Bold',
+    'bold_italic' => $rootDir . '/lib/fonts/DejaVuSans-BoldOblique',
+    'italic' => $rootDir . '/lib/fonts/DejaVuSans-Oblique',
+    'normal' => $rootDir . '/lib/fonts/DejaVuSans',
+  ),
+  'dejavu sans mono' => array(
+    'bold' => $rootDir . '/lib/fonts/DejaVuSansMono-Bold',
+    'bold_italic' => $rootDir . '/lib/fonts/DejaVuSansMono-BoldOblique',
+    'italic' => $rootDir . '/lib/fonts/DejaVuSansMono-Oblique',
+    'normal' => $rootDir . '/lib/fonts/DejaVuSansMono',
+  ),
+  'dejavu serif' => array(
+    'bold' => $rootDir . '/lib/fonts/DejaVuSerif-Bold',
+    'bold_italic' => $rootDir . '/lib/fonts/DejaVuSerif-BoldItalic',
+    'italic' => $rootDir . '/lib/fonts/DejaVuSerif-Italic',
+    'normal' => $rootDir . '/lib/fonts/DejaVuSerif',
+  ),
+  'material design icons' => array(
+    'normal' => $fontDir . '/material_design_icons_normal_e00dbfb1d34b20ec2ab8e484833bef16',
+  ),
+  'boxicons' => array(
+    'normal' => $fontDir . '/boxicons_normal_233defe05975d700e447039fbb530f6e',
+  ),
+  'font awesome 5 brands' => array(
+    'normal' => $fontDir . '/font_awesome__5_brands_normal_5369d096163f7eea1869562163909715',
+  ),
+  'font awesome 5 free' => array(
+    'normal' => $fontDir . '/font_awesome__5_free_normal_2a28549a42558ae377df18ff7be02503',
+    '900' => $fontDir . '/font_awesome__5_free_900_98b62535138b7f3d04b55e734dedccf7',
+  ),
+  'dripicons-v2' => array(
+    'normal' => $fontDir . '/dripicons_v2_normal_fdc6b3f8a5e9275a3dbef058b019d8df',
+  ),
+  'font awesome\\ 5 brands' => array(
+    'normal' => $fontDir . '/font_awesome__5_brands_normal_5369d096163f7eea1869562163909715',
+  ),
+  'font awesome\\ 5 free' => array(
+    'normal' => $fontDir . '/font_awesome__5_free_normal_2a28549a42558ae377df18ff7be02503',
+    '900' => $fontDir . '/font_awesome__5_free_900_98b62535138b7f3d04b55e734dedccf7',
+  ),
+) ?>
---
+++ storage/fonts/material_design_icons_normal_e00dbfb1d34b20ec2ab8e484833bef16.ufm.php
@@ -0,0 +1,29 @@
+<?php return array (
+  'codeToName' => 
+  array (
+  ),
+  'isUnicode' => true,
+  'EncodingScheme' => 'FontSpecific',
+  'FontName' => 'Material Design Icons',
+  'FullName' => 'Material Design Icons',
+  'Version' => 'Version 1.0',
+  'PostScriptName' => 'MaterialDesignIcons',
+  'Weight' => 'Medium',
+  'ItalicAngle' => '0',
+  'IsFixedPitch' => 'false',
+  'UnderlineThickness' => '0',
+  'UnderlinePosition' => '10',
+  'FontHeightOffset' => '0',
+  'Ascender' => '875',
+  'Descender' => '-125',
+  'FontBBox' => 
+  array (
+    0 => '-2',
+    1 => '-129',
+    2 => '1834',
+    3 => '885',
+  ),
+  'CIDtoGID_Compressed' => true,
+  'CIDtoGID' => 'eJztwTEBAAAAwqD1T+1hDaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAbgAeAAE=',
+  '_version_' => 6,
+);
